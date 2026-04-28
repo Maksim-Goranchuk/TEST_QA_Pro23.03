6aa21ffc10b489a6f89215b654c3cb630d317811
Maksim <qa@wandry.com.ua>
2026-04-28 16:17:24 +0300

    TEST

---
+++ TEST_WORK/TEST.js
@@ -0,0 +1,18 @@
+
+// // var Pi = 3.1415;
+
+// // function side(r){
+// //     let S = r* r* Pi
+// //     result = S
+// //     return result
+// // }
+// // console.log(side(15))
+// var productPrice1 = 101.5;
+// var productPrice2 = 60.25;
+// var productPrice3 = 10.11;
+
+
+// var totalSum = productPrice1 + productPrice2 + productPrice3;
+// var averagePrice = totalSum / 3;
+
+// console.log("Сред " + averagePrice.toFixed(2));
